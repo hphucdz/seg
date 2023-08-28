9e05c1032837b659c0f51d0c854f0487b078a3cc
hphucdz <143400828+hphucdz@users.noreply.github.com>
2023-08-28 17:24:10 +0700

    Add files via upload

---
+++ Data.lua
@@ -0,0 +1,16 @@
+-- NhpHub --
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "KaySexHub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "Player",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+	Name = "Section"
+})
+
